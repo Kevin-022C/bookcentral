4c87c73ca96c37b16c4ee24cccf4388a1e45aaa8
Kevin-022C <keyfrans96@gmail.com>
2025-12-17 12:38:26 -0500

    1. inicio proyecto

---
+++ src/View/ListaLibrosView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ListaLibros />
+  </div>
+</template>
+
+<script>
+import ListaLibros from '@/components/ListaLibros.vue';
+export default {
+    components: {
+        ListaLibros
+    }
+}
+</script>
+
+<style>
+
+</style>
